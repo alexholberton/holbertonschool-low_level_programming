c2fc38c043caf5cd160fa88e51cfd3edb50133a7
alexholberton <13059@holbertonstudents.com>
2026-02-24 16:15:01 +0100

    print combinations

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+* Description: prints all combinations of single-digit numbers
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		putchar(i + '0');
+		if (i < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		i++;
+	}
+	putchar('\n');
+
+	return (0);
+}
