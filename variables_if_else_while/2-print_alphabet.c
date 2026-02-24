29b72e4d4b59287bcb7aec41cb5981071467c748
alexholberton <13059@holbertonstudents.com>
2026-02-24 10:45:29 +0100

    print alphabet

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+	* main - Entry point
+	* Description: Print the alphabet in lowercase, followed by a new line.
+	* Return: Always 0 (Success)
+	*/
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	for (n = 'a'; n <= 'z'; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
