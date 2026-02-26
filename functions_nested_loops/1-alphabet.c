d2855af7909c2d1d27327d0878f81679d0d4b054
alexholberton <13059@holbertonstudents.com>
2026-02-26 16:56:51 +0100

    alphabet

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * Description: Prints the alphabet in lowercase, followed by a new line.
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		_putchar(letter);
+	}
+	    _putchar('\n');
+
+	return (0);
+}
