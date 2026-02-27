d1913c2277cb81ccf36754240b343f5be30b10d0
alexholberton <13059@holbertonstudents.com>
2026-02-27 14:02:51 +0100

    add

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * add - adds two intergers and returns the result 
+ * @a : first interger
+ * @b : second interger
+ * Return: the result of the addition
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+ 
