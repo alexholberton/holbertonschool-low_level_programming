1ba31f7dec2a45d3c3844329370b0150c11efd11
alexholberton <13059@holbertonstudents.com>
2026-02-27 14:53:17 +0100

    fix sum_to_n.c

--- intro_debugging/sum_to_n.c
+++ intro_debugging/sum_to_n.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
 
-	int sum_to_n(int n)
+int sum_to_n(int n)
+{
+    int i;
+    int sum = 0;
 
-	{
-	    int i;
+    for (i = 1; i <= n; i++)
+        sum += i;
 
-	    int sum = 0;
+    return sum;
+}
 
+/* -- DO NOT Modify the code below this line -- */
 
-	    for (i = 1; i <= n; i++)
-	        sum += i;
-		return (sum);
-	}
-
-	int main(void)
-	{
-	    printf("%d\n", sum_to_n(10));
-	    return (0);
-	}
+int main(void)
+{
+    printf("%d\n", sum_to_n(10));
+    return 0;
+}
