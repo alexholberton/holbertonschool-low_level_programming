541aaaf958bf232b30288a210ec3907c289acff9
alexholberton <13059@holbertonstudents.com>
2026-03-02 15:45:42 +0100

    calculator.c

--- simple-calculator/calculator.c
+++ simple-calculator/calculator.c
@@ -1,55 +1,30 @@
 #include <stdio.h>
 
 /**
- * main - entry point for the simple calculator
+ * main - Entry point of the calculator program
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
 	int choice;
-	int a, b;
 
-	do {
-		printf("Simple Calculator\n");
-		printf("1) Add\n");
-		printf("2) Subtract\n");
-		printf("3) Multiply\n");
-		printf("4) Divide\n");
-		printf("0) Quit\n");
-		printf("Choice: ");
+	int a, b, result;
 
-		if (scanf("%d", &choice) != 1)
-		{
-			while (getchar() != '\n')
-				;
-			continue;
-		}
+	printf("Choice: ");
+	scanf("%d", &choice);
 
-		if (choice == 1)
-		{
-			/* Task 2: Addition Logic */
-			printf("A: ");
-			scanf("%d", &a);
-			printf("B: ");
-			scanf("%d", &b);
-			printf("Result: %d\n", a + b);
-		}
-		else if (choice == 0)
-		{
-			printf("Bye!\n");
-		}
-		else if (choice >= 2 && choice <= 4)
-		{
-			/* Placeholders for next tasks */
-			continue;
-		}
-		else
-		{
-			printf("Invalid choice\n");
-		}
+	if (choice == 2)
+	{
+		printf("A: ");
+		scanf("%d", &a);
+		printf("B: ");
+		scanf("%d", &b);
 
-	} while (choice != 0);
+		result = a - b;
+
+		printf("Result: %d\n", result);
+	}
 
 	return (0);
 }
