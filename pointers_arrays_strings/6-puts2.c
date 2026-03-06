b4fc003873ef2d5d78c7da2da4bad17a97ddde7a
alexholberton <13059@holbertonstudents.com>
2026-03-06 15:50:02 +0100

    6-puts2.c

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -1,22 +1,29 @@
 #include "main.h"
 
 /**
- * _puts2 - prints every other character of a string, followed by a new line
- * @str: pointer to the string to print
- *
- * Return: void
- */
+* _puts2 - prints every other character of a string
+* @str: pointer to the string to print
+*
+* Return: void
+*/
 void _puts2(char *str)
 {
 	int i = 0;
 
+	/* On vérifie que la chaîne n'est pas vide dès le début */
 	while (str[i] != '\0')
 	{
-		if (i % 2 == 0)
+		_putchar(str[i]);
+
+		/* On saute un caractère. Si on arrive au bout, on s'arrête. */
+		if (str[i + 1] == '\0')
 		{
-			_putchar(str[i]);
+			i++;
 		}
-		i++;
+		else
+		{
+			i += 2;
+		}
 	}
 	_putchar('\n');
 }
