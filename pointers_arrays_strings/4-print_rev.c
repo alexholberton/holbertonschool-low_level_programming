629298d7c32a1673678b971a8f61b82c46593a8a
alexholberton <13059@holbertonstudents.com>
2026-03-06 15:27:16 +0100

    fix print rev

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -1,16 +1,23 @@
 #include "main.h"
 
 /**
- * print_rev - prints a string in reverse, followed by a new line
- * @s: pointer to the string to print in reverse
- *
- * Return: void
- */
+* print_rev - prints a string in reverse
+* @s: pointer to the string to print
+*
+* Return: void
+*/
 void print_rev(char *s)
+
 {
-	int len = _strlen(s);
+	int len = 0;
+
 	int i;
 
+	/* Calculer la longueur manuellement si _strlen n'est pas dispo */
+	while (s[len] != '\0')
+		len++;
+
+	/* Affichage en sens inverse */
 	for (i = len - 1; i >= 0; i--)
 	{
 		_putchar(s[i]);
