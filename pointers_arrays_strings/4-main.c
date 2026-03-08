af2878594a55b7733aae22ebf4f5da3176796220
alexholberton <13059@holbertonstudents.com>
2026-03-08 19:13:40 +0100

    4-main.c

--- pointers_arrays_strings/4-main.c
+++ pointers_arrays_strings/4-main.c
@@ -1,15 +1,20 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * main - check the code
- *
- * Return: Always 0.
- */
+* main - check the code
+*
+* Return: Always 0.
+*/
 int main(void)
 {
-    char *str;
+	char *s = "hello, world";
 
-    str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
-    print_rev(str);
-    return (0);
+	char *f = "world";
+
+	char *t;
+
+	t = _strpbrk(s, f);
+	printf("%s\n", t);
+	return (0);
 }
