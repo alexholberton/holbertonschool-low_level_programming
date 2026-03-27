d5b4661b6f290a294dc897d3784d1eda081f599b
alexholberton <13059@holbertonstudents.com>
2026-03-27 09:04:02 +0100

    fix 0 print list.c

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -2,29 +2,24 @@
 #include "lists.h"
 
 /**
- * print_list - Affiche tous les éléments d'une liste list_t.
- * @h: Pointeur vers le début de la liste (le premier noeud).
+ * print_list - prints all the elements of a list_t list
+ * @h: pointer to the list_t list to print
  *
- * Return: Le nombre total de noeuds dans la liste.
+ * Return: the number of nodes
  */
 size_t print_list(const list_t *h)
 {
 	size_t nodes = 0;
 
-	while (h != NULL)
+	while (h)
 	{
-		if (h->str == NULL)
-		{
+		if (!h->str)
 			printf("[0] (nil)\n");
-		}
 		else
-		{
 			printf("[%u] %s\n", h->len, h->str);
-		}
-
-		h = h->next;
 
 		nodes++;
+		h = h->next;
 	}
 
 	return (nodes);
