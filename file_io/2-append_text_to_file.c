162666bb983df876674d2fb0d320de894f7b228f
alexholberton <13059@holbertonstudents.com>
2026-04-10 12:34:23 +0200

    fix 2-append_text_to_file.c

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -5,7 +5,7 @@
 /**
  * append_text_to_file - Ajoute du texte à la fin d'un fichier existant.
  * @filename: Nom du fichier.
- * @text_content: Chaîne de caractères à ajouter (terminée par NULL).
+ * @text_content: Chaîne de caractères à ajouter.
  *
  * Return: 1 en cas de succès, -1 en cas d'échec.
  */
@@ -18,20 +18,17 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	/* 1. Ouvrir le fichier en mode écriture seule et APPEND */
-	/* Note : On ne met pas de permissions (0600) car on ne crée pas le fichier */
+	/* Ouverture en mode ecriture seule et ajout (APPEND) */
 	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd == -1)
 		return (-1);
 
-	/* 2. Si text_content est NULL, on a juste vérifié que le fichier existe */
 	if (text_content != NULL)
 	{
-		/* Calcul de la longueur de la chaîne */
+		/* Calcul de la longueur de la chaine */
 		while (text_content[len])
 			len++;
 
-		/* 3. Écrire à la fin du fichier */
 		bytes_written = write(fd, text_content, len);
 		if (bytes_written == -1 || bytes_written != len)
 		{
